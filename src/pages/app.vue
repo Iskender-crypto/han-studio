d025ce7567ad2e93c33fea47b8cecd23b50a0457
fuzayil-ahmadaliev <fuzayilahmadaliev00@gmail.com>
2024-10-29 13:03:51 +0600

    Commit

--- src/pages/app.vue
+++ src/pages/app.vue
@@ -1,41 +1,74 @@
 
 <template>
-  <div class="card flex justify-center">
-    <Button label="Show" @click="visible = true" />
-    <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
-      <p class="mb-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p class="mb-8">
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-        adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
-        ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
-      <p class="mb-8">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae
-        non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-      </p>
-      <p class="mb-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p class="mb-8">
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-        adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
-        ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae
-        non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-      </p>
-    </Dialog>
+  <div class="container">
+    <div class="big">
+    <div style="display: flex;flex-direction: column; border: 2px solid #535bf2 !important;" class="col1">
+      <div style="display:flex;flex-direction: column;align-items: start" class="block">
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_1">
+        <h3>Single Developer</h3>
+        <p>For individual developers</p>
+          </div>
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_2">
+          <h3>$199</h3>
+        </div>
+        <div style="display: flex;flex-direction: column;align-items: start;margin-bottom: 25px; margin-top: 20px" class="block_3">
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">1 Developer</p></div>
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">All Marketing + Application UI Blocks</p></div>
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">Figma UI Kit</p></div>
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">Lifetime Support</p></div>
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">nlimited Updates</p></div>
+          <div style="display: flex;align-items: center" ><i class="pi pi-check" style="color:slateblue"></i><p style="margin-left: 10px">Use on Unlimited Projects</p></div>
+        </div>
+      </div>
+      <div class="BUTTONS">
+      <button style=" margin-bottom: 20px;border-radius: 2px;color: #f9f9f9;background: #535bf2;width: 70%">Buy Now</button>
+    </div>
+    </div>
+    <div style="display: flex;flex-direction: column;border:2px solid  gold !important;" class="col1">
+      <div style="display:flex;flex-direction: column;align-items: start" class="block">
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_1">
+        <h3>Team</h3>
+        <p>For small teams</p>
+          </div>
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_2">
+          <h3>$699</h3>
+        </div>
+        <div style="display: flex;flex-direction: column;align-items: start;margin-bottom: 25px; margin-top: 20px" class="block_3">
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">Up to 25 Developers</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">All Marketing + Application UI Blocks</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">Figma UI Kit</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">Lifetime Support</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">Unlimited Updates</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue;"></i><p style="margin-left: 10px">Use on Unlimited Projects</p></div>
+        </div>
+      </div>
+      <div class="BUTTONS">
+      <button style=" margin-bottom: 20px;border-radius: 2px;color: #f9f9f9;background: gold ;width: 70%">Buy Now</button>
+    </div>
+    </div>
+    <div style="display: flex;flex-direction: column;border: 2px solid dimgrey !important;" class="col1">
+      <div style="display:flex;flex-direction: column;align-items: start" class="block">
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_1">
+        <h3>Enterprise</h3>
+        <p>For larger teams</p>
+          </div>
+        <div style="box-shadow: -2px 15px 8px -14px rgba(34, 60, 80, 0.68);display: flex;flex-direction: column;align-items: start" class="block_2">
+          <h3>Exclusive Deals</h3>
+        </div>
+        <div style="display: flex;flex-direction: column;align-items: start;margin-bottom: 25px; margin-top: 20px" class="block_3">
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">Unlimited Developers</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">All Marketing + Application UI Blocks</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">Figma UI Kit</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">Lifetime Support</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">Unlimited Updates</p></div>
+          <div style="display: flex;align-items: center"> <i class="pi pi-check" style="color: slateblue"></i><p style="margin-left: 10px">Use on Unlimited Projects</p></div>
+        </div>
+      </div>
+      <div class="BUTTONS">
+      <button style=" margin-bottom: 20px;border-radius: 2px;color: #f9f9f9;background: dimgrey ;width: 70%">Contact Us</button>
+    </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -47,44 +80,77 @@ const visible = ref(false);
 
 
 <style scoped>
+.BUTTONS{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 .big{
+  width: 100%;
+  height: 700px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   flex-wrap: wrap;
 }
-.container{
-  width: 1140px;
-  margin: 0 auto;
-  padding: 0;
+.col1{
+  width: 30.333%;
+  background: #ffffff;
+  justify-content:space-between;
+  border-radius: 10px;
+  display: flex;flex-direction: column;
+  border: red 5px !important;
+}
+.block{
+padding-left: 20px;
 }
 
+.container{
+width: 1140px;
+margin: 0 auto;
+padding: 0;
+}
 @media screen and (max-width: 1140px) {
-  .container{
-    width: 992px;
-  }
+.container{
+width: 992px;
+}
 }
 @media screen and (max-width: 992px) {
-  .container{
-    width: 776px;
-  }
+.container{
+width: 776px;
+}
 }
 @media screen and (max-width: 776px) {
-  .container{
-    width: 576px;
+.container{
+width: 576px;
+}
+  .col1{
+    width: 48% !important;
+    flex-wrap: wrap;
+    margin-bottom: 30px
   }
 }
 @media screen and (max-width: 576px) {
-  .container{
-    width: 414px;
-  }
+.container{
+width: 414px;
+}
+  .col1{
+   width: 100% !important;
+   flex-wrap: wrap;
+    margin-bottom: 40px;
+ }
 }
 @media screen and (max-width: 414px) {
-  .container{
-    width: 320px;
-  }
+.container{
+width: 320px;
+}
 }
 @media screen and (max-width: 320px) {
-  .container{
-    width: 100%;
-    padding: 5px 10px;
-  }
+.container{
+width: 100%;
+padding: 5px 10px;
+}
 }
 </style>
